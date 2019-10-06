fd9079b5139c5b9cf652b8fbc5068f2abc152e0d
Ben <biller010@gmail.com>
2019-10-05 21:36:30 -0600

    Extract Method from child class to parent class
    - child class contained a utility method that better belonged to parent class so every sub class could benefit

--- player/Player.java
+++ player/Player.java
@@ -1,7 +1,7 @@
 package player;
 
 import coordinates.Coordinates;
-
+import board.Board;
 import java.io.Serializable;
 // board piece enum
 import board.Board.BoardPiece;
@@ -13,10 +13,32 @@ import board.Board.BoardPiece;
  * Date of last change: Apr. 9, 2017
  */
 public abstract class Player implements Serializable {
+	
 	/**
 	 * Prompt user for input, validate input, tell user if they hit or if they missed
 	 * @param targetBoard; an array containing the opponent's ship locations
 	 * @return a Coordinates object representing the location targeted by the user
 	 */
-	public abstract Coordinates getMove(BoardPiece[][] targetBoard);
+	public abstract Coordinates getMove(Board targetBoard);
+	
+	/*
+	 * checks if a coordinate is available
+	 * 
+	 * returns true if the coordinate is either empty or a ship, else it returns false
+	 */
+	public static boolean isLegalMove(Coordinates coord, BoardPiece[][] playerBoard) {
+		boolean validity = true;
+		if (!coord.isInBounds()) {
+			validity = false;
+		}
+		
+		int row = coord.getRow();
+		int col = coord.getCol();
+		if (playerBoard[row][col] != BoardPiece.SHIP && playerBoard[row][col] != BoardPiece.EMPTY) {
+			validity = false;
+		}
+		
+		return validity;
+	}
+	
 }
